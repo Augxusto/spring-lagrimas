6fb5b8481a5a31fd190f2f56858ef14b71df3fe9
Augxusto <rodrigo.silva.110805@gmail.com>
2026-02-26 21:23:54 -0300

    firts commit

---
+++ src/main/java/com/rdg/lagrimas/LagrimasApplication.java
@@ -0,0 +1,13 @@
+package com.rdg.lagrimas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagrimasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagrimasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdg/lagrimas/LagrimasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdg.lagrimas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagrimasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
