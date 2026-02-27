8c17e40bc3f33d10187dd3c4cbfb8fc4c1c2cb0e
Augxusto <rodrigo.silva.110805@gmail.com>
2026-02-26 21:59:18 -0300

    real service, o anterior era o repository

---
+++ src/main/java/com/rdg/lagrimas/service/EstudanteService.java
@@ -0,0 +1,32 @@
+package com.rdg.lagrimas.service;
+
+import com.rdg.lagrimas.entity.Estudante;
+import com.rdg.lagrimas.repository.EstudanteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EstudanteService {
+
+    @Autowired
+    private EstudanteRepository repository;
+
+    public Estudante salvar(Estudante estudante) {
+        return repository.save(estudante);
+    }
+
+    public List<Estudante> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Estudante> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+    
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
